1733dec976534e796d4244dbd81a4fc4b8c22667
Mantas M <mantasef@gmail.com>
2022-06-09 17:45:47 +0300

    beggin router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import MainNav from './components/MainNav';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <MainNav />
+      <Switch>
+        <Route path='/about'>
+          <AboutPage />
+        </Route>
+        <Route path='/contact'>
+          <ContactPage />
+        </Route>
+        <Route path='/'>
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
